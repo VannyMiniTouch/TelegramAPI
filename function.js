119c054893b896bf6d6bb7468701a3fd33b8831b
VannyMiniTouch <VannyMiniTouch@gmail.com>
2022-09-08 10:50:35 +0700

    update js

--- function.js
+++ function.js
@@ -1,76 +1,76 @@
 
-    $(document).ready(function () {
+$(document).ready(function () {
 
-        const Tele_web = window.Telegram.WebApp;
-        console.log(Tele_web)
-
-
-
-        $('#btn_main').on('click', function () {
-            Tele_web.MainButton.text = "Main Button Click";
-            Tele_web.MainButton.show();
-        });
+    const Tele_web = window.Telegram.WebApp;
+    console.log(Tele_web)
 
 
 
-        $('#btn_back').on('click', function () {
-            Tele_web.BackButton.show();
-        });
-        
-        if(!window.location.pathname){
-            Tele_web.BackButton.show();
-        }
+    $('#btn_main').on('click', function () {
+        Tele_web.MainButton.text = "Main Button Click";
+        Tele_web.MainButton.show();
+    });
 
 
 
-        window.Telegram?.WebApp.MainButton.onClick(() => {
-            //   window.Telegram.WebApp.sendData(selectedRegions);
-            alert("main button clicked");
-        });
+    $('#btn_back').on('click', function () {
+        Tele_web.BackButton.show();
+    });
 
-        Tele_web.BackButton.onClick(() => {
-            history.back();
-        })
-
-
-
-        if(!window.location.pathname){
-            Tele_web.BackButton.show();
-        }
+    if (!window.location.pathname) {
+        Tele_web.BackButton.show();
+    }
 
 
 
+    window.Telegram?.WebApp.MainButton.onClick(() => {
+        //   window.Telegram.WebApp.sendData(selectedRegions);
+        alert("main button clicked");
+    });
 
-
-
-
-
-
-
-
-
-        // $('#test_btn').html(JSON.stringify(Tele_web))
-
-        // mainButton is the VIEW ORDER button in the image.
-        // const mainButton = window.Telegram.WebApp.MainButton;
-        // mainButton.text = `View Order, or whatever you wish`;
-        // mainButton.enable();
-        // mainButton.show();
-        // mainButton.onClick(() => { handleClick, or handleSubmit });
-
-
-
-
-        // Telegram.WebApp.onEvent('mainButtonClicked', function(){
-        //     $('#test_btn').html="Main btn click"
-        //     consol.log("test")
-        // })
-
-
-        // Tele_web.MainButton.onClick(function(){
-        //     $('#test_btn').html="Main btn click"
-        //     consol.log("test")
-        // });
-
+    Tele_web.BackButton.onClick(() => {
+        history.back();
     })
+
+
+
+    if (!window.location.pathname) {
+        Tele_web.BackButton.show();
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+    // $('#test_btn').html(JSON.stringify(Tele_web))
+
+    // mainButton is the VIEW ORDER button in the image.
+    // const mainButton = window.Telegram.WebApp.MainButton;
+    // mainButton.text = `View Order, or whatever you wish`;
+    // mainButton.enable();
+    // mainButton.show();
+    // mainButton.onClick(() => { handleClick, or handleSubmit });
+
+
+
+
+    // Telegram.WebApp.onEvent('mainButtonClicked', function(){
+    //     $('#test_btn').html="Main btn click"
+    //     consol.log("test")
+    // })
+
+
+    // Tele_web.MainButton.onClick(function(){
+    //     $('#test_btn').html="Main btn click"
+    //     consol.log("test")
+    // });
+
+})
 
